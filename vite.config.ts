2b7dc9b46cab3304f989d7cfdaddac04a166a8fa
yurie <futatsugi.y@gmail.com>
2026-09-20 20:29:08 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
